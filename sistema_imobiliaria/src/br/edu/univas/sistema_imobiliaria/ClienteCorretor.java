40fc4e1498672f0ecf3da47a7616b26cbb48b877
Jean Souza <jeanvictorspuza2015@gmail.com>
2024-09-25 21:53:01 -0300

    Implementação da classe Cliente e Classe ClienteCorretor

---
+++ sistema_imobiliaria/src/br/edu/univas/sistema_imobiliaria/ClienteCorretor.java
@@ -0,0 +1,33 @@
+package br.edu.univas.sistema_imobiliaria;
+
+public class ClienteCorretor {
+    private int codCliente;
+    private int codCorretor;
+
+    public ClienteCorretor(int codCliente, int codCorretor) {
+        this.codCliente = codCliente;
+        this.codCorretor = codCorretor;
+    }
+
+    // Getters e Setters
+    public int getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public int getCodCorretor() {
+        return codCorretor;
+    }
+
+    public void setCodCorretor(int codCorretor) {
+        this.codCorretor = codCorretor;
+    }
+
+    public void exibirRelacao() {
+        System.out.println("Código do Cliente: " + codCliente);
+        System.out.println("Código do Corretor: " + codCorretor);
+    }
+}
