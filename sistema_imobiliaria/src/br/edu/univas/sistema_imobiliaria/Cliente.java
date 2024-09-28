33f1fb6f3bee8606884ac592c107e56e3c32364d
Jean Souza <jeanvictorspuza2015@gmail.com>
2024-09-28 20:30:32 -0300

    Atualização da classe cliente, getter e setters

--- sistema_imobiliaria/src/br/edu/univas/sistema_imobiliaria/Cliente.java
+++ sistema_imobiliaria/src/br/edu/univas/sistema_imobiliaria/Cliente.java
@@ -16,6 +16,41 @@ public class Cliente {
         this.cpf = cpf;
         this.cnpj = cnpj;
     }
-
-    
+    //getter setters
+    public int getCod() {
+        return cod;
+    }
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public long getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
